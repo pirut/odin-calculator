55f287da7695d5ae84f9a7eafc6dbb36f93021ed
JR Bussard <scottbussardjr@gmail.com>
2024-08-05 20:14:23 -0400

    added error handling to the eval function and added calculator obj

--- index.js
+++ index.js
@@ -9,16 +9,29 @@ function addNumber(currentNumber, adderNumber) {
 }
 
 function evaluateNumbers(operation) {
-    switch (operation.operator) {
-        case "add":
-            return operation.first + operation.second;
-        case "subtract":
-            return operation.first - operation.second;
-        case "multiply":
-            return operation.first * operation.second;
-        case "divide":
-            return operation.first / operation.second;
-        default:
-            return null;
+    if (operation.first === null && operation.second === null) {
+        return 0;
+    } else if (operation.first !== null && operation.second === null) {
+        return operation.first;
+    } else {
+        switch (operation.operator) {
+            case "add":
+                return operation.first + operation.second;
+            case "subtract":
+                return operation.first - operation.second;
+            case "multiply":
+                return operation.first * operation.second;
+            case "divide":
+                return operation.first / operation.second;
+            default:
+                return null;
+        }
     }
 }
+
+let calculator = {
+    first: null,
+    second: null,
+    operator: null,
+    result: null,
+};
